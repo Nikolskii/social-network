a78f9d24ff2032ea8bfc63ff70decc9498ecd03c
Denis Nikolskiy <den-apple@ya.ru>
2022-11-17 20:19:04 +0200

    feat: создать компонент Header

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import '../index.css';
+
+function App() {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="https://nikolskii.github.io/russian-travel/images/header-logo.svg"
+        alt="Логотип"
+      />
+    </header>
+  );
+}
+
+export default Header;
